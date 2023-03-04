164b9e67ea1d8e8ddc4a73e9d8ef3bcf056174c7
Esteban Canteros <estebanacanteros@gmail.com>
2023-03-04 14:53:39 -0300

    add product page base mock

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -1,4 +1,5 @@
 import { IProduct } from '@/interfaces'
+import NextLink from 'next/link'
 import { Grid, Card, CardActionArea, CardMedia, Box, Typography } from '@mui/material'
 import { useMemo, useState } from 'react'
 
@@ -16,9 +17,11 @@ const Product = ({ product }: Props) => {
   return (
     <Grid item xs={6} sm={4} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
       <Card>
-        <CardActionArea>
-          <CardMedia  className='fadeIn' component='img' image={productImage} alt={product.title} />
-        </CardActionArea>
+        <NextLink href='/products/slug' passHref prefetch={false}>
+          <CardActionArea>
+            <CardMedia className='fadeIn' component='img' image={productImage} alt={product.title} />
+          </CardActionArea>
+        </NextLink>
       </Card>
       <Box sx={{ mt: 1 }} className='fadeIn'>
         <Typography fontWeight={700}>{product.title}</Typography>
---
+++ src/pages/products/slug.tsx
@@ -0,0 +1,45 @@
+import { Grid, Box, Typography, Button, Chip } from '@mui/material'
+
+import { ShopLayout } from '../../components/layouts'
+import { initialData } from '../../database/products'
+
+const productTest = initialData.products[0]
+
+const ProductPage = () => {
+  return (
+    <ShopLayout pageDescription={productTest.description} title={productTest.title}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={7}>
+          {/* Slideshow */}
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          <Box display='flex' flexDirection='column'>
+            <Typography variant='h1' component='h1'>
+              {productTest.title}
+            </Typography>
+            <Typography variant='subtitle1' component='h2'>
+              ${productTest.price}
+            </Typography>
+            <Box sx={{ my: 2 }}>
+              <Typography variant='subtitle2'>Amount: </Typography>
+              {/* Item counter component */}
+            </Box>
+
+            {/* Add to cart */}
+            <Button color='secondary' className='circular-btn'>
+              Add to cart
+            </Button>
+            <Chip label='Out of stock' color='error' variant='outlined' />
+
+            <Box sx={{ mt: 3 }}>
+              <Typography variant='subtitle2'>Description</Typography>
+              <Typography variant='body2'>{productTest.description}</Typography>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ShopLayout>
+  )
+}
+
+export default ProductPage
