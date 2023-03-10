2aa0675c180a1f8792c67b6964a91441adab02b3
Esteban Canteros <estebanacanteros@gmail.com>
2023-03-10 09:24:06 -0300

    add LoginPAge design

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,40 @@
+import NextLink from 'next/link'
+import { AuthLayout } from '@/components/layouts'
+import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material'
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <AuthLayout title='Login'>
+      <Box sx={{ width: 350, padding: '10px 20px' }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant='h1' component='h1'>
+              Login
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label='Email' variant='filled' fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label='Password' type='password' variant='filled' fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <Button color='secondary' className='circular-btn' size='large' fullWidth>
+              Login
+            </Button>
+          </Grid>
+          <Grid item xs={12} display='flex' justifyContent='end'>
+            <NextLink href='/auth/register' passHref>
+              <Link component='span' underline='always'>
+                You don&apos;t have an account?
+              </Link>
+            </NextLink>
+          </Grid>
+        </Grid>
+      </Box>
+    </AuthLayout>
+  )
+}
+
+export default LoginPage
