9f8a4c0275b7c8db6ab889902b6753f14cda7714
Esteban Canteros <estebanacanteros@gmail.com>
2023-03-15 10:18:47 -0300

    installation and testing SWR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import { ProductList } from '@/components/products'
-import { initialData } from '@/database/products'
 import { Typography } from '@mui/material'
 import { ShopLayout } from '../components/layouts'
 
+import useSWR from 'swr'
+const fetcher = (...args: [key: string]) => fetch(...args).then((res) => res.json())
+
 export default function Home() {
+  const { data, error, isLoading } = useSWR('/api/products', fetcher, {})
+
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+
   return (
     <ShopLayout title='Teslo-Shop - Home' pageDescription='Find the best Teslo Products'>
       <Typography variant='h1' component='h1'>
@@ -12,7 +19,7 @@ export default function Home() {
       <Typography variant='h2' sx={{ mb: 1 }}>
         All the products
       </Typography>
-      <ProductList products={initialData.products as any} />
+      <ProductList products={data} />
     </ShopLayout>
   )
 }
