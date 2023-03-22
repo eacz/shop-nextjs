938a7409eae609609b7f78ac1259482272def7f4
Esteban Canteros <estebanacanteros@gmail.com>
2023-03-22 09:58:18 -0300

    little optimization to show productCard with a fadeIn entirely

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -9,6 +9,7 @@ interface Props {
 
 const Product = ({ product }: Props) => {
   const [isHovered, setIsHovered] = useState(false)
+  const [isImageLoaded, setIsImageLoaded] = useState(false)
 
   const productImage = useMemo(() => {
     return isHovered ? `products/${product.images[1]}` : `products/${product.images[0]}`
@@ -19,11 +20,17 @@ const Product = ({ product }: Props) => {
       <Card>
         <NextLink href='/products/slug' passHref prefetch={false}>
           <CardActionArea>
-            <CardMedia className='fadeIn' component='img' image={productImage} alt={product.title} />
+            <CardMedia
+              className='fadeIn'
+              component='img'
+              image={productImage}
+              alt={product.title}
+              onLoad={() => setIsImageLoaded(true)}
+            />
           </CardActionArea>
         </NextLink>
       </Card>
-      <Box sx={{ mt: 1 }} className='fadeIn'>
+      <Box sx={{ mt: 1, display: isImageLoaded ? 'block' : 'none' }} className='fadeIn'>
         <Typography fontWeight={700}>{product.title}</Typography>
         <Typography fontWeight={500}>${product.price}</Typography>
       </Box>
