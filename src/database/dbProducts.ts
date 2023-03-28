c1a5afa4015c6e0de81c6e642cc58558b98d756e
Esteban Canteros <estebanacanteros@gmail.com>
2023-03-28 16:16:35 -0300

    show other products if there is no match on user search

--- src/database/dbProducts.ts
+++ src/database/dbProducts.ts
@@ -2,8 +2,8 @@ import { Product } from '@/models'
 import { db } from '@/database'
 import { IProduct } from '../interfaces/product'
 
-{
-}
+type ReducedProduct = Pick<IProduct, 'title' | 'images' | 'price' | 'inStock' | 'slug'>
+
 export const getProductBySlug = async (slug: String): Promise<IProduct | null> => {
   await db.connect()
   const product = await Product.findOne({ slug }).lean()
@@ -28,8 +28,6 @@ export const getAllProductsSlugs = async (): Promise<ProductSlug[]> => {
   return slugs
 }
 
-type ReducedProduct = Pick<IProduct, 'title' | 'images' | 'price' | 'inStock' | 'slug'>
-
 export const getProductsByTerm = async (term: string = ''): Promise<ReducedProduct[]> => {
   term = term.toString().toLowerCase()
   await db.connect()
@@ -40,6 +38,13 @@ export const getProductsByTerm = async (term: string = ''): Promise<ReducedProdu
     .select('title images price inStock slug -_id')
     .lean()
 
+  await db.disconnect()
+  return JSON.parse(JSON.stringify(products))
+}
+
+export const getAllProducts = async (): Promise<ReducedProduct[]> => {
+  await db.connect()
+  const products = await Product.find().select('title images price inStock slug -_id')
   await db.disconnect()
   return JSON.parse(JSON.stringify(products))
 }
--- src/pages/search/[query].tsx
+++ src/pages/search/[query].tsx
@@ -1,24 +1,38 @@
-import { Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { GetServerSideProps } from 'next'
 
 import { ProductList } from '@/components/products'
 import { ShopLayout } from '@/components/layouts'
 import { dbProducts } from '@/database'
-import { IProduct } from '../../interfaces/product'
+import { IProduct } from '@/interfaces/product'
 
 interface Props {
   products: IProduct[]
+  wereProductsFounded: boolean
+  query: string
 }
 
-export default function Search({ products }: Props) {
+export default function Search({ products, query, wereProductsFounded }: Props) {
   return (
     <ShopLayout title='Teslo-Shop - Search' pageDescription='Find the best Teslo Products'>
       <Typography variant='h1' component='h1'>
         Search Products
       </Typography>
-      <Typography variant='h2' sx={{ mb: 1 }}>
-        query:
-      </Typography>
+      {wereProductsFounded ? (
+        <Typography color='secondary' variant='h2' sx={{ mb: 1 }}>
+          {query}
+        </Typography>
+      ) : (
+        <Box display='flex'>
+          <Typography variant='h2' sx={{ mb: 1 }}>
+            There is no products that match your search
+          </Typography>
+          <Typography color='secondary' variant='h2' sx={{ mb: 1, ml:1 }}>
+            {query}
+          </Typography>
+        </Box>
+      )}
+
       <ProductList products={products} />
     </ShopLayout>
   )
@@ -31,10 +45,13 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     return { redirect: { destination: '/', permanent: false } }
   }
   let products = await dbProducts.getProductsByTerm(query)
+  const wereProductsFounded = products.length > 0
 
-  //TODO: if there is no match with the search, return other product that the user may find interesting
+  if(!wereProductsFounded){
+    products = await dbProducts.getAllProducts()
+  }
 
   return {
-    props: { products },
+    props: { products, query, wereProductsFounded },
   }
 }
