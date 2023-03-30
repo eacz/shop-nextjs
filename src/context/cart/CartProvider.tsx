8156cf1cb2657ee683390814445e2648a21060d6
Esteban Canteros <estebanacanteros@gmail.com>
2023-03-30 18:30:19 -0300

    store products in cart in a cookie, and load them on refresh/first load of the page

--- src/context/cart/CartProvider.tsx
+++ src/context/cart/CartProvider.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, useReducer } from 'react'
+import { ReactNode, useEffect, useReducer } from 'react'
+import Cookie from 'js-cookie'
 
 import { CartContext, CartReducer } from '.'
 import { ICartProduct } from '@/interfaces'
@@ -14,13 +15,30 @@ const CART_INITIAL_STATE: CartState = {
 export const CartProvider = ({ children }: { children: ReactNode }) => {
   const [state, dispatch] = useReducer(CartReducer, CART_INITIAL_STATE)
 
+  useEffect(() => {
+    try {
+      const cartItems = JSON.parse(Cookie.get('cart') || '[]')
+      dispatch({ type: '[Cart] - LoadCart from cookies', payload: cartItems })
+    } catch (error) {
+      dispatch({ type: '[Cart] - LoadCart from cookies', payload: [] })
+    }
+  }, [])
+
+  useEffect(() => {
+    if (state.cart.length > 0) {
+      Cookie.set('cart', JSON.stringify(state.cart))
+    }
+  }, [state.cart])
+
   const addProductToCart = (product: ICartProduct) => {
     const isProductInCart = state.cart.some((p) => p._id === product._id)
     if (!isProductInCart) {
       return dispatch({ type: '[Cart] - Update Cart Products', payload: [...state.cart, product] })
     }
 
-    const productInCartButDifferentSize = state.cart.some((p) => p._id === product._id && p.size === product.size)
+    const productInCartButDifferentSize = state.cart.some(
+      (p) => p._id === product._id && p.size === product.size
+    )
     if (!productInCartButDifferentSize) {
       return dispatch({ type: '[Cart] - Update Cart Products', payload: [...state.cart, product] })
     }
--- src/context/cart/CartReducer.ts
+++ src/context/cart/CartReducer.ts
@@ -2,16 +2,15 @@ import { CartState } from './CartProvider'
 import { ICartProduct } from '../../interfaces/cart'
 
 type cartActionType =
-  | { type: '[Cart] - LoadCart from cookies | storage'; payload: ICartProduct[] }
+  | { type: '[Cart] - LoadCart from cookies'; payload: ICartProduct[] }
   | { type: '[Cart] - Update Cart Products'; payload: ICartProduct[] }
 
 export const CartReducer = (state: CartState, action: cartActionType): CartState => {
-
   switch (action.type) {
     case '[Cart] - Update Cart Products':
-      console.log(action.payload);
-      
-      return { ...state, cart: [...action.payload ]}
+      return { ...state, cart: [...action.payload] }
+    case '[Cart] - LoadCart from cookies':
+      return { ...state, cart: action.payload }
     default:
       return state
   }
