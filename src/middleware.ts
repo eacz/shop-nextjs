11acdf8cf610ece0e46035a3b6318e285b528621
Esteban Canteros <estebanacanteros@gmail.com>
2023-04-30 17:46:01 -0300

    update middleware ts to work with next auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server'
-import * as jose from 'jose'
+import { getToken } from 'next-auth/jwt'
 
 export async function middleware(req: NextRequest) {
   if (req.nextUrl.pathname.startsWith('/checkout')) {
-    const token = req.cookies.get('token')?.value
+    const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
 
-    try {
-      await jose.jwtVerify(token || '', new TextEncoder().encode(process.env.SECRET_KEY || ''))
-      //If no error is thrown, the JWT is valid, you can even the payload if necessary
-      return NextResponse.next()
-    } catch (error) {
-      console.error(`JWT Invalid or not signed in`, { error })
-      const { protocol, host, pathname } = req.nextUrl
-      return NextResponse.redirect(`${protocol}//${host}/auth/login?page=${pathname}`)
+    if (!session) {
+      const requestedPage = req.nextUrl.pathname
+      const url = req.nextUrl.clone()
+      url.pathname = `/auth/login`
+      url.search = `page=${requestedPage}`
+
+      return NextResponse.redirect(url)
     }
+
+    return NextResponse.next()
   }
 }
 // Only the paths declared in here will run the middleware
 export const config = {
-  matcher: ['/checkout/:path*'],
+  matcher: ['/checkout/address', '/checkout/summary'],
 }
